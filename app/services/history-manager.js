81defc02f8272dba1d15a4447c61bc81bcde0a04
OmarMasarweh <masyomar@gmail.com>
2013-09-18 13:22:21 +0200

    Added removeLink

--- app/services/history-manager.js
+++ app/services/history-manager.js
@@ -1,27 +1,42 @@
-﻿
 app.service("HistoryManager", function () {
+	// Array Remove - By John Resig (MIT Licensed)
+	Array.prototype.remove = function(from, to) {
+		if(from == -1)
+			return;
+		var rest = this.slice((to || from) + 1 || this.length);
+  		this.length = from < 0 ? this.length + from : from;
+  		return this.push.apply(this, rest);
+	};
 
-    Array.prototype.pushUnique = function (item) {
-        if (this.indexOf(item) == -1) {
-            this.push(item);
-            return true;
-        }
+	function pushUnique(arr, item) {
+		var i = arr.indexOf(item);
+		if (i != -1) {
+			arr.remove(i);
+		}
+		arr.unshift(item);
+		return arr;
+	}
 
-        return false;
-    }
+    	function addLink(url) {
+		var links = getLinks();
+		links = pushUnique(links,url);
+		localStorage["ODataLinks"] = JSON.stringify(links);
+    	}
 
-    function addLink(url) {
-        var links = getLinks();
-        links.pushUnique(url);
-        localStorage["ODataLinks"] = JSON.stringify(links);
-    }
+	function removeLink(url){
+		var links = getLinks();
+		var i = links.indexOf(url);
+		links.remove(i);
+		localStorage["ODataLinks"] = JSON.stringify(links);
+	}
 
-    function getLinks() {
-        return JSON.parse(localStorage["ODataLinks"] || '[]');
-    }
+    	function getLinks() {
+        	return JSON.parse(localStorage["ODataLinks"] || '[]');
+    	}
 
-    return {
-        getLinks: getLinks,
-        addLink: addLink
-    }
+    	return {
+        	getLinks: getLinks,
+		addLink: addLink,
+		removeLink: removeLink
+    	}
 });
