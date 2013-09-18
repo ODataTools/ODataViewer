a766596a7a36f6121b2fd3a3611e967400fd7589
Ghassan-Fadila <f.ghassan@gmail.com>
2013-09-18 12:07:50 +0200

    append correction
    issuses solved: build the final string after =

--- app-plugins/intellisense/directive/intellisense.js
+++ app-plugins/intellisense/directive/intellisense.js
@@ -42,23 +42,26 @@ angular.module('Intellisense').directive('intellisense', function () {
                 };
 
 
-                function append(str, intel) {
-                    var parts = str.split('?');
-
-                    if (parts.length == 1) {
-                        return addIntel(str, intel, '/');
-                    }
-
-                    var lastQuery = parts[1].split('&').pop();
-                    var queryParts = lastQuery.split('=');
-
-                    if (queryParts.length == 1) {
-                        return parts[0] + '?' + addIntel(parts[1], intel, '&');
-                    }
-                    return parts[0] + '?' + addIntel(parts[1], intel, ' ');
-                }
-
-                function addIntel(str, intel, sep) {
+                function append(str, intel){
+					var parts = str.split('?');
+	
+					if(parts.length == 1){
+						return addIntel(str, intel, '/');
+					}
+	
+					var lastQuery = parts[1].split('&').pop();
+					var queryParts = lastQuery.split('=');
+	
+					if(queryParts.length == 1){
+						return parts[0] + '?' +  addIntel(parts[1], intel, '&');
+					}
+	
+					lastQueryArr = lastQuery.split('=');
+	
+					return parts[0] + '?' + parts[1].join('&') + '&'+ lastQueryArr[0] + '=' + addIntel(lastQueryArr[1], intel, ' ');
+				}
+                
+				function addIntel(str, intel, sep) {
                     var tmp = (sep === ' ') ? str.split(/\s+/) : str.split(sep);
                     tmp.pop();
                     tmp.push(intel);
