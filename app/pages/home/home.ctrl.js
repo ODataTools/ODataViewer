ee2d13d35bb64e73bbf85b4510f3f4a8d4354320
Hasan Abo-Shally <HasanAboShally@gmail.com>
2013-09-28 01:19:10 +0300

    style #8 + team

--- app/pages/home/home.ctrl.js
+++ app/pages/home/home.ctrl.js
@@ -15,7 +15,40 @@ app.controller("HomeCtrl", function ($scope, $http, $routeParams, HistoryManager
 
 
 
-   // $scope.team = [];
+    $scope.team = [
+        {
+            //<a href='http://dataservicestool.codeplex.com/'></a>
+            name: "Eyal Vardi (Mentor)",
+            photoUrl: "resources/photos/color/eyal.jpg",
+            linkedin: "http://www.linkedin.com/profile/view?id=18109570",
+            description: "Eyal has a windows based application for viewing OData, and he came up with the idea of making a web-based application for helping developers using OData.<br />Eyal mentored the project and helped the team with several technical issues."
+        },
+        {
+            name: "Hasan Abo-Shally",
+            photoUrl: "resources/photos/color/hasan.jpg",
+            linkedin: "http://www.linkedin.com/profile/view?id=183930974",
+            description: "Hasan led the team developing the project.<br />He worked on the application's structure, the user interface, and the integration of the addons into the tool."
+        },
+        {
+            name: "Ghassan Fadila",
+            photoUrl: "resources/photos/color/ghassan.jpg",
+            //linkedin: "http://www.linkedin.com/pub/hakam-kittany/26/930/128",
+            description: "Ghassan worked on the intellisense plugin that provided suggestions for the auto complete feature based on the given url, he also helped other team members with their work."
+        },
+        {
+            name: "Hakam Kittany",
+            photoUrl: "resources/photos/color/hakam.jpg",
+            linkedin: "http://www.linkedin.com/pub/hakam-kittany/26/930/128",
+            description: "Hakam worked on the tree and entity-graph plugins for displaying the meta-data, and wrapped them into AngularJS directives."
+        }
+        ,
+        {
+            name: "Mohammad Abu Shah",
+            photoUrl: "resources/photos/color/shah.jpg",
+            //linkedin: "http://www.linkedin.com/pub/hakam-kittany/26/930/128",
+            description: "Mohammad worked on the testing of the intellisense plugin."
+        }
+    ];
 
 
     $scope.isSelectedLink = function (link) {
